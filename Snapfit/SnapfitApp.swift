b852afe3eafb5823d6b0f0c8d4eafd56496f4f24
Isaac Almanza <isaac.almanza19@gmail.com>
2025-05-07 13:51:13 -0500

    Initial Commit

---
+++ Snapfit/SnapfitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapfitApp.swift
+//  Snapfit
+//
+//  Created by Isaac Almanza on 05/07/25.
+//
+
+import SwiftUI
+
+@main
+struct SnapfitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnapfitTests/SnapfitTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnapfitTests.swift
+//  SnapfitTests
+//
+//  Created by Isaac Almanza on 05/07/25.
+//
+
+import Testing
+@testable import Snapfit
+
+struct SnapfitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
